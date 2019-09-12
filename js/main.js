e8780918e1185d776de68ce75d7c54aedf68d4ac
GEETHANJALI MARIMUTHU contractor <geethanjali.marimuthu@contractor.tatacommunications.com>
2019-09-13 00:29:32 +0530

    initial push

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var deadline = '31/12/2015';
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(classname, endtime){
+  var clock = document.getElementsByClassName('clockdiv')[i];
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock(){
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock,1000);
+}
+
+var deadline = 'December 31 2015 00:00:50 UTC+0200';
+var y=document.querySelectorAll('.clockdiv').length;
+for (var i=0;i<12;i++){
+initializeClock('clockdiv', deadline);
+}
